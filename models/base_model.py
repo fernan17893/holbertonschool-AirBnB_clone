aa9c147a4b12492c206b2404870b9432e560851b
fernan17893 <fernan17893@gmail.com>
2022-06-29 16:10:30 +0000

    task 3 done, task 4 unfinished

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-                
+        if kwargs:
+            for k, v in kwargs.items:
+                if k != __class__:
+                    setattr(self, k, v)                
 
     def __str__(self):
         """ prints """
-        return (f"[BaseModel] (<self.id>) <self.__dict__>")
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
         """ updates updated_at with current datetime """
