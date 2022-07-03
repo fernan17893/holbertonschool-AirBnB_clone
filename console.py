1c07452cc2bffe2e30614705c8e0a3b93d8f0bab
Andy <3775@holbertonschool.com>
2022-07-03 06:51:54 +0000

    Debug

--- console.py
+++ console.py
@@ -27,7 +27,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Creates a new instance of BaseClass with error handling"""
+        """Creates a new instance of BaseClass with error handling\n"""
         if arg == '':
             print('** class name missing **')
         elif arg not in self.__classes:
@@ -38,8 +38,7 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the class\
-name and id. Ex: $ show BaseModel 1234-1234-1234"""
+        """Prints the string rep of an instance based on the class and ID\n"""
         args = arg.split()
         if arg == '':
             print('** class name missing **') 
@@ -52,6 +51,22 @@ name and id. Ex: $ show BaseModel 1234-1234-1234"""
         else:
             name = f"{args[0]}.{args[1]}"
             print(storage.all()[name])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the classname and ID\n"""
+        args = arg.split()
+        if arg == '':
+            print('** class name missing **') 
+        elif args[0] not in self.__classes:
+            print('** class doesn\'t exist **')
+        elif len(args) < 2:
+            print('** instance id missing **')
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print('** no instance found **')
+        else:
+            name = f"{args[0]}.{args[1]}"
+            storage.all().pop(name)
+            storage.save()
 
 
 
