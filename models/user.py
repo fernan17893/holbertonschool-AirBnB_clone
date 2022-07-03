50e61ac1734cc27073bbf3cd08eed5e198911ef4
Andy <3775@holbertonschool.com>
2022-07-03 10:02:24 +0000

    Debug

--- models/user.py
+++ models/user.py
@@ -12,10 +12,6 @@ class User(BaseModel):
     first_name = ''
     last_name = ''
 
-    def __init__(self, email='', password='', first_name='', last_name='', *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Method called on initialization of new user"""
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
         super().__init__()
