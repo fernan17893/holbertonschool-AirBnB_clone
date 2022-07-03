6e2f08fdbe0926275e1fac55834601cc67cf3c58
Andy <3775@holbertonschool.com>
2022-07-03 06:34:36 +0000

    Add parts of console

--- console.py
+++ console.py
@@ -1,34 +1,59 @@
 #!/usr/bin/env python3
 """ Entry point of command interpreter """
 import cmd, sys
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Shell class for HBNB project"""
 
     prompt = '(hbnb) '
 
+    __classes = {
+        "BaseModel": BaseModel()
+    }
+
     #basic commands
-    def do_quit(self, arg):
-        'Quit the shell\n'
+    def do_quit(self, args):
+        """Quit the shell\n"""
         exit()
 
     def do_EOF(self, arg):
-        'Quit the shell\n'
+        """Quit the shell\n"""
         exit()
 
     def emptyline(self):
-        'Do nothing in case of empty line\n'
+        """Do nothing in case of empty line\n"""
         pass
 
-#help is provided by default but must be updated and documented
+    def do_create(self, arg):
+        """Creates a new instance of BaseClass with error handling"""
+        if arg == '':
+            print('** class name missing **')
+        elif arg not in self.__classes:
+            print('** class doesn\'t exist **')
+        else:
+            obj = self.__classes[arg]
+            storage.new(obj)
+            storage.save()
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class\
+name and id. Ex: $ show BaseModel 1234-1234-1234"""
+        args = arg.split()
+        if arg == '':
+            print('** class name missing **') 
+        elif args[0] not in self.__classes:
+            print('** class doesn\'t exist **')
+        elif len(args) < 2:
+            print('** instance id missing **')
+        elif f"{args[0]}.{args[1]}" not in storage.all():
+            print('** no instance found **')
+        else:
+            name = f"{args[0]}.{args[1]}"
+            print(storage.all()[name])
 
 
-#empty line + ENTER should not execute anything
-
-
-
-## quit and EOF to exit the program
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ t.py
@@ -0,0 +1,7 @@
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+m1 = BaseModel()
+m2 = BaseModel()
+m3 = BaseModel()
+s = FileStorage()
+d = {'a': 1, 'b': 2, 'c': 3}
